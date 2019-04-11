3783980ca4bd358d149deb0999b68594420afe9e
Junyoung McArdle <juny.mcardle@gmail.com>
2019-04-11 18:00:01 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,7 @@ class Transfer
 
   def execute_transaction
     if @sender.balance < @amount
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
 
