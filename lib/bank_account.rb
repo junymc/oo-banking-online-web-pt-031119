7d27ef0018bc68e326d01af2d554bb0eb7d0e766
Junyoung McArdle <juny.mcardle@gmail.com>
2019-04-11 17:28:30 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,24 @@
 class BankAccount
 
+  attr_accessor :balance, :status
+  def initialize(account)
+    @account = account
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
 end
